f86bd1f33002122bb37ce1cd233242b94905a3cc
MateuszBieda <mateusbieda@gmail.com>
2023-04-11 19:33:50 +0200

    loadMoreBtn.classList.add('is-hidden');loadMoreBtn.classList.add('is-hidden');

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ loadMoreBtn.addEventListener('click', onLoadMoreBtn);
 
 function handleSubmit(e){
     e.preventDefault();
-
+    let page = 1;
      const descrImage = e.currentTarget.searchQuery.value.trim();
      gallery.innerHTML='';
      const URL = `?key=${API_KEY}&q=${descrImage}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${page}`;
@@ -45,19 +45,21 @@ function handleSubmit(e){
     fetchGallery(descrImage, page, perPage)
     .then((response) => {
         //gallery.innerHTML='';
-        loadMoreBtn.classList.add('is-hidden');
+        loadMoreBtn.classList.remove('is-hidden');
         //console.log(response.data);
         renderImageList(response);
         const totalHits=response.data.totalHits;
+        galleryList.refresh();
         //console.log(totalHits);
         if(totalHits===0){
             failureMessage();
             gallery.innerHTML='';
+            loadMoreBtn.classList.add('is-hidden');
         }
-        if(totalHits>perPage){
-            loadMoreBtn.classList.remove('is-hidden');
-            onLoadMoreBtn();
-        }
+        // if(totalHits>perPage){
+        //     loadMoreBtn.classList.remove('is-hidden');
+        //     onLoadMoreBtn();
+        // }
         if(totalHits>0){
             succesMessage(totalHits);
             const { height: cardHeight } = document
@@ -71,6 +73,8 @@ function handleSubmit(e){
 
           
         }
+
+        
         
     })
     .catch(error => console.log(error));
@@ -137,9 +141,11 @@ function handleSubmit(e){
     
     page += 1;
     //galleryList.destroy();
-    
+    const descrImage = input.value.trim();
+     gallery.innerHTML='';
     fetchGallery(descrImage, page, perPage)
       .then((response) => {
+        console.log(response.data.hits);
         renderImageList(response);
         const totalHits=response.data.totalHits;
         galleryList.refresh();
@@ -153,98 +159,3 @@ function handleSubmit(e){
       })
       .catch(error => console.log(error));
   }
-
-        //   function searchImage (e){
-        //     e.preventDefault();
-        //     descrImage = e.currentTarget.searchQuery.value.trim();
-        //     console.log(descrImage);
-        //     getImage(descrImage);
-        // };
-
-
-
-//===============================================TEST==============================================================================
-
-
-// const fetchUsers = async () => {
-//     const response = await fetch("https://pixabay.com/api/?key=35005985-6320445dd5945a516c4e799c6&q=yellow+flowers&image_type=photo&pretty=true&id=2295434");
-//     const data = await response.json();
-//     return data;
-//   };
-  
-//   fetchUsers().then(data => console.log(data));
-
-
-//  async function getImage() {
-//             try {
-//               const response = await axios.get("https://pixabay.com/api/?key=35005985-6320445dd5945a516c4e799c6&q=yellow+flowers&image_type=photo&pretty=true&id=2295434");
-//               console.log(response);
-//             } catch (error) {
-//               console.error("hello");
-//             }
-//           };
-//           getImage().then(response =>console.log(response.data));
-
-
-
-///////////////TEST1------correct//////////////////////////////
-
-//button.addEventListener("click", fetchData());
-
-    // function fetchData () {
-    //     axios.get("https://pixabay.com/api/?key=35005985-6320445dd5945a516c4e799c6&q=yellow+flowers&image_type=photo&pretty=true&id=2295434")
-    //     .then(function(response){
-    //         console.log(response.data.hits);
-
-    //     })
-    //     .catch(function(error){
-    //         console.log(error);
-    //     })
-
-    // };
-///////////////TEST2 ---correct //////////////////////////////
-
-    // button.addEventListener("click", fetchImage());
-    // async function fetchImage() {
-    //     try {
-    //       const response = await axios.get('https://pixabay.com/api/?key=35005985-6320445dd5945a516c4e799c6&q=yellow+flowers&image_type=photo&pretty=true&id=2295434');
-    //       console.log(response.data.hits);
-    //     } catch (error) {
-    //       console.error(error);
-    //     }
-    //   }
-
-////////////////////////////////////////copy
-
-// function renderImageList(descrImage){
-
-//     const markup = image.style.width='200';
-//     .map(({webformatURL , largeImageURL , tags, likes, views, comments, downloads }) =>
-//     `<div class="photo-card">
-//     <img src="${Object.values(webformatURL)}" alt="${Object.values(tags)}" loading="lazy" />
-//     <div class="info">
-//     <p class="info-item">
-//     <b>Likes</b>
-//     ${Object.values(likes)}
-//     </p>
-//     <p class="info-item">
-//     <b>Views</b>
-//     ${Object.values(views)}
-//     </p>
-//     <p class="info-item">
-//     <b>Comments</b>
-//     ${Object.values(comments)}
-//     </p>
-//     <p class="info-item">
-//     <b>Downloads</b>
-//     ${Object.values(downloads)}
-//     </p>
-//     </div>
-//     </div>`)
-//     .join("");
-//     console.log(Object.values(tags));
-//     return console.log(markup);
-    
-//     gallery.insertAdjacentHTML('beforeend',markup);
-
-//    }
